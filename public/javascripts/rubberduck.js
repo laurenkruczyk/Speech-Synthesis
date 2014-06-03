581310c90d1a2267c21e694be58244de9074948c
Samuel McTaggart <sam.mctaggart@gmail.com>
2014-06-03 21:55:49 +0100

    tidy up code and make it speak

--- public/javascripts/rubberduck.js
+++ public/javascripts/rubberduck.js
@@ -14,65 +14,7 @@ $(document).ready(function() {
     var elemlength = list.length;
     var randomnum = Math.floor(Math.random()*elemlength);
     var randomitem = list[randomnum];
-
-    if (input.toLowerCase().indexOf("regex") > -1) {
-      var quote = $("<blockquote>").text(input);
-      $("#placeholder").before(quote);
-      $("#regex").clone().insertBefore("#placeholder").fadeIn(1000);
-    }
-
-    else if (input.toLowerCase().indexOf("ruby") > -1) {
-      var quote = $("<blockquote>").text(input);
-      $("#placeholder").before(quote);
-      $("#ruby").clone().insertBefore("#placeholder").fadeIn(1000);
-    }
-
-    else if (input.toLowerCase().indexOf("rails") > -1) {
-      var quote = $("<blockquote>").text(input);
-      $("#placeholder").before(quote);
-      $("#rails").clone().insertBefore("#placeholder").fadeIn(1000);
-    }
-
-    else if (input.toLowerCase().indexOf("jq") > -1) {
-      var quote = $("<blockquote>").text(input);
-      $("#placeholder").before(quote);
-      $("#jquery").clone().insertBefore("#placeholder").fadeIn(1000);
-    }
-
-    else if (input != "jargon") {
-      var quote = $("<blockquote>").text(input);
-      $("#placeholder").before(quote);
-      $(randomitem).clone().insertBefore("#placeholder").fadeIn(1000);
-    }
-
-    var msg = new SpeechSynthesisUtterance($(randomitem).text());
-    msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Whisper'; })[0];
-      msg.voice = voices[10]; // Note: some voices don't support altering params
-      msg.voiceURI = 'native';
-      msg.volume = 1; // 0 to 1
-      msg.rate = 2; // 0.1 to 10
-      msg.pitch = 2; //0 to 2
-      msg.text = 'Hello World';
-      msg.lang = 'en-US';window.speechSynthesis.speak(msg);
-      var voices = window.speechSynthesis.getVoices();
-
-     speechSynthesis.getVoices().forEach(function(voice) {
-      console.log(voice.name, voice.default ? '(default)' :'');
-    });
-   
-    speechSynthesis.speak(msg);
-
-      msg.onend = function(e) {
-      console.log('Finished in ' + event.elapsedTime + ' seconds.');
-    };
-
-    // try {
-    // var msg = new SpeechSynthesisUtterance($(randomitem).text());
-    // window.speechSynthesis.speak(msg);
-    // } catch(e) {
-    //   console.log('oh ho')
-    // }
-    
+    answer(input.toLowerCase());
     $("#console").scrollTop($("#console")[0].scrollHeight);
     $("#command_line").val("");
     });
@@ -80,5 +22,30 @@ $(document).ready(function() {
   // $(randomitem).css("display", "block");
 });
 
+function speak(message, voice) {
+  var voice = voice || 0;
+  var msg = new SpeechSynthesisUtterance(message);
+  var voices = speechSynthesis.getVoices();
+  msg.voice = voices[0];
+  speechSynthesis.speak(msg);
+}
 
+function answer(input) {
+  var keyword = words.filter(function(word){return input.indexOf(word) != -1})[0];
+  var quote = $("<blockquote>").text(input);
+  $("#placeholder").before(quote);
+  if (!!keyword) {
+    var response = $("#" + keyword);
+  } else {
+    var response = $(randomItem());
+  }
+  response.clone().insertBefore("#placeholder").fadeIn(1000);
+  speak(response.text());
+}
 
+function randomItem() {
+  var list = $("ul li").toArray();
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+var words = ['regex', 'ruby', 'rails', 'jq']
