6793d7fcd00cd6d7c8f0c165c4c1f87126a32028
lauren.kruczyk <lauren.kruczyk@gmail.com>
2014-06-03 15:56:25 -0400

    no luck changing voice yet

--- public/javascripts/rubberduck.js
+++ public/javascripts/rubberduck.js
@@ -44,16 +44,41 @@ $(document).ready(function() {
       $("#placeholder").before(quote);
       $(randomitem).clone().insertBefore("#placeholder").fadeIn(1000);
     }
-    try {
+
     var msg = new SpeechSynthesisUtterance($(randomitem).text());
-    window.speechSynthesis.speak(msg);
-    } catch(e) {
-      console.log('oh ho')
-    }
+    msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Whisper'; })[0];
+      msg.voice = voices[10]; // Note: some voices don't support altering params
+      msg.voiceURI = 'native';
+      msg.volume = 1; // 0 to 1
+      msg.rate = 2; // 0.1 to 10
+      msg.pitch = 2; //0 to 2
+      msg.text = 'Hello World';
+      msg.lang = 'en-US';window.speechSynthesis.speak(msg);
+      var voices = window.speechSynthesis.getVoices();
+
+     speechSynthesis.getVoices().forEach(function(voice) {
+      console.log(voice.name, voice.default ? '(default)' :'');
+    });
+   
+    speechSynthesis.speak(msg);
+
+      msg.onend = function(e) {
+      console.log('Finished in ' + event.elapsedTime + ' seconds.');
+    };
+
+    // try {
+    // var msg = new SpeechSynthesisUtterance($(randomitem).text());
+    // window.speechSynthesis.speak(msg);
+    // } catch(e) {
+    //   console.log('oh ho')
+    // }
+    
     $("#console").scrollTop($("#console")[0].scrollHeight);
     $("#command_line").val("");
     });
 
   // $(randomitem).css("display", "block");
 });
+
+
 
